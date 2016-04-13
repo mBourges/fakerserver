c212be60578a1008be9f3ffb942c4de2f54e150d
Matt <m.bourges@gmail.com>
2016-04-13 14:16:08 +0000

    set for public key

--- deploy/setSSHKey.sh
+++ deploy/setSSHKey.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo rhc sshkey add test ~/.ssh/id_1cd48cb6e5f6834a0a00c05468272116 --confirm -l $OPENSHIFT_USERNAME -p $OPENSHIFT_PWD
+sudo rhc sshkey add test ~/.ssh/id_1cd48cb6e5f6834a0a00c05468272116.pub --confirm -l $OPENSHIFT_USERNAME -p $OPENSHIFT_PWD
