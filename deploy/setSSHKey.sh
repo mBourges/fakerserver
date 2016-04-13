1659a712c50f4eafb89695111e5623d5c024fa41
Matt <m.bourges@gmail.com>
2016-04-13 14:06:23 +0000

    added sudo to script

--- deploy/setSSHKey.sh
+++ deploy/setSSHKey.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rhc sshkey add test ~/.ssh/id_1cd48cb6e5f6834a0a00c05468272116 -l $OPENSHIFT_USERNAME -p $OPENSHIFT_PWD
+sudo rhc sshkey add test ~/.ssh/id_1cd48cb6e5f6834a0a00c05468272116 -l $OPENSHIFT_USERNAME -p $OPENSHIFT_PWD
