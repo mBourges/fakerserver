d3e0eba3f78f1b5bdf5d7713cec0243f33b90ebb
Matt <m.bourges@gmail.com>
2016-04-14 07:34:21 +0000

    deploy script changed

--- deploy/setSSHKey.sh
+++ deploy/setSSHKey.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
-sudo rhc sshkey add circleci ~/.ssh/id_3d3bfac8e0c37a8be801de77debcb7bf.pub --confirm -l $OPENSHIFT_USERNAME -p $OPENSHIFT_PWD
+sudo rhc sshkey add circleci ~/.ssh/id_getrandom-appmonster.rhcloud.com.pub --confirm -l $OPENSHIFT_USERNAME -p $OPENSHIFT_PWD
 sudo git clone ssh://570cc1f97628e1a2e70001f8@getrandom-appmonster.rhcloud.com/~/git/getrandom.git/
 # cd getrandom
 #git remote add upstream git@github.com:mBourges/fakerserver.git
