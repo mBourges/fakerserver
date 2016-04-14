e16c40a29a5e4ad762a1e596e2e32c472f223779
Matt <m.bourges@gmail.com>
2016-04-14 07:05:19 +0000

    test

--- deploy/setSSHKey.sh
+++ deploy/setSSHKey.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo rhc sshkey add circleci ~/.ssh/id_068c295400be05bbe0b93cc0e464ba57.pub --confirm -l $OPENSHIFT_USERNAME -p $OPENSHIFT_PWD
+ssh -i ~/.ssh/id_068c295400be05bbe0b93cc0e464ba57 appmonster.rhcloud.com
 sudo git clone ssh://570cc1f97628e1a2e70001f8@getrandom-appmonster.rhcloud.com/~/git/getrandom.git/
 # cd getrandom
 #git remote add upstream git@github.com:mBourges/fakerserver.git
